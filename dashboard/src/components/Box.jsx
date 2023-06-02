db8d72990ee3491ed5658004f6419aa0a8b09de0
Kingkarzy <43390918+Kingkarzy@users.noreply.github.com>
2023-06-02 17:45:11 +0100

    Withdrawal Page and Type Writer dev dep

--- dashboard/src/components/Box.jsx
+++ dashboard/src/components/Box.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
-export const Box = ({ children }) => {
+
+const Box = ({ children }) => {
   return (
     <div className="w-4/5 h-auto mx-auto bg-white p-5 flex flex-col rounded-sm shadow-2xl">
       {children}
     </div>
   );
 };
+export default Box;
 
 export const UseBox = ({ width, height, children }) => {
   return (
--- dashboard/src/pages/MyAccount/Balance.jsx
+++ dashboard/src/pages/MyAccount/Balance.jsx
@@ -1,8 +1,15 @@
 
 function Balance() {
   return (
-    <div>Balance</div>
-  )
+    <div>
+      <div className="black-gradient flex p-4 justify-end">
+        <h1 className="h1">Account Balance</h1>
+      </div>
+      <div>
+      
+      </div>
+    </div>
+  );
 }
 
 export default Balance
