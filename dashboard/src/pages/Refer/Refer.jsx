36e8ce55ca7cbde9396b79bee7b23444469aa05c
Kingkarzy <43390918+Kingkarzy@users.noreply.github.com>
2023-06-02 22:56:41 +0100

    Trade Page

--- dashboard/src/pages/Refer/Refer.jsx
+++ dashboard/src/pages/Refer/Refer.jsx
@@ -1,9 +1,47 @@
-
-
+import {Box} from "../../components/Box"
 function Refer() {
   return (
-    <div>Refer</div>
-  )
+    <div className="h-screen flex flex-col gap-5">
+      <div className="black-gradient flex p-4 justify-end">
+        <h1 className="h2">Refer Users to Apex Trading Services Community</h1>
+      </div>
+      <div>
+        <Box>
+          <h2 className="font:semibold">You can refer users by sharing your referral link:</h2>
+          <div>[http://apexaitrading.com/ref/{1644}]</div>
+        </Box>
+      </div>
+      <div>
+        <Box>
+          <h2>[Your Sponsor]</h2>
+          <div>[Avatar]</div>
+          <div>[null]</div>
+        </Box>
+      </div>
+      <div>
+        <table className="table w-full my-5 border text-center border-solid border-gray-100">
+          <thead className="bg-white">
+            <tr>
+              <th>Client Name</th>
+              <th>Client Inv. Plan</th>
+              <th>Client Status</th>
+              <th>Ref Level</th>
+              <th>Date Registered</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{1}</td>
+              <td>{200}</td>
+              <td>{"BTC"}</td>
+              <td>{"Paid"}</td>
+              <td>{"20-10-2023"}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Refer
+export default Refer;
--- dashboard/src/pages/Trade/Trade.jsx
+++ dashboard/src/pages/Trade/Trade.jsx
@@ -1,5 +1,86 @@
+import PriceCard from "../../components/PricingCard";
 function Trade() {
-  return <div>Trade</div>;
+  return (
+    <div className="items-center justify-evenly my-5">
+      <div className="black-gradient flex p-4 justify-evenly">
+        <h1 className="h2">Available Packages</h1>
+      </div>
+      <div className="flex justify-center bg-blue-300 py-2 mb-5">
+        <p className="text-blue-900 ">
+          &#9888;
+          {" You do not have a package at the moment"}
+        </p>
+      </div>
+      <div className="flex flex-wrap gap-5">
+        <PriceCard
+          heading={"Bronze"}
+          price="$1000"
+          percent="25% daily"
+          duration="Duration: 5 days"
+          refer="2% referral bonus"
+          content={`Capital accessible after investment elapses.`}
+          // bgColor='#4182AB'
+          width="17rem"
+          height={400}
+        />
+        <PriceCard
+          heading={"Silver"}
+          price="$5000"
+          percent="35% daily"
+          duration="Duration: 7 days"
+          refer="2% referral bonus"
+          content={`Capital accessible after investment elapses.`}
+          // bgColor='#4182AB'
+          width="17rem"
+          height={400}
+        />
+        <PriceCard
+          heading={"Gold"}
+          price="$10000"
+          percent="40% daily"
+          duration="Duration: 10 days"
+          refer="5% referral bonus"
+          content={`Capital accessible after investment elapses.`}
+          // bgColor='#4182AB'
+          width="17rem"
+          height={400}
+        />
+        <PriceCard
+          heading={"Diamond"}
+          price="$25000"
+          percent="50% daily"
+          duration="Duration: 14 days"
+          refer="5% referral bonus"
+          content={`Capital accessible after investment elapses.`}
+          // bgColor='#4182AB'
+          width="17rem"
+          height={400}
+        />
+        <PriceCard
+          heading={"Emerald"}
+          price="$50000"
+          percent="55% daily"
+          duration="Duration: 20 days"
+          refer="8% referral bonus"
+          content={`Capital accessible after investment elapses.`}
+          // bgColor='#4182AB'
+          width="17rem"
+          height={400}
+        />
+        <PriceCard
+          heading={"Ruby"}
+          price="$100000"
+          percent="75% daily"
+          duration="Duration: 25days"
+          refer="8% referral bonus"
+          content={`Capital accessible after investment elapses.`}
+          // bgColor='#4182AB'
+          width="17rem"
+          height={400}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Trade;
