423bd9ee248c1825c8192dfc935e87305f21552a
ciijay99 <chigozieahanwa99@gmail.com>
2023-06-03 00:24:33 +0100

    Set Modal for subscription page

--- dashboard/src/pages/Subscription/Subscription.jsx
+++ dashboard/src/pages/Subscription/Subscription.jsx
@@ -1,12 +1,16 @@
+import { Modal } from '@mui/material';
 import { OutlineButton } from '../../components/Button';
 import { PrimaryButton } from '../../components/Button';
+import { useState } from 'react';
 
 function Subscription() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div className='flex flex-col gap-10 md:px-10 h-[75vh] overflow-x-hidden md:w-full overflow-y-scroll'>
       <div className='black-gradient flex flex-col p-4 items-end'>
         <h1 className='h1'>Subscription Trade</h1>
-        <p className='text-white'>Refer users to ApexAI</p>
       </div>
       <div className='bg-white flex flex-col px-5 py-7 w-full lg:w-8/12 shadow-3xl'>
         <p className='font-normal text-sm md:text-base'>
@@ -19,11 +23,23 @@ function Subscription() {
         </p>
       </div>
       <div className='w-2/4 gap-5 flex'>
-        <PrimaryButton className='w-1/12'>
-          Subscribe Now
-        </PrimaryButton>
-        <OutlineButton>Submit MT4 Details</OutlineButton>
+        <div>
+          <PrimaryButton className='w-1/12'>
+            Subscribe Now
+          </PrimaryButton>
+        </div>
+        <div onClick={handleOpen}>
+          <OutlineButton>Submit MT4 Details</OutlineButton>
+        </div>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <div></div>
+      </Modal>
     </div>
   );
 }
