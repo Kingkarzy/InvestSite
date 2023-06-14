19e2aeeebdead5ec40558a2e6c42c42c61d4589c
ciijay99 <chigozieahanwa99@gmail.com>
2023-06-14 16:14:40 +0100

    Final Updates After Merge

--- client/src/components/LazyLoad.jsx
+++ client/src/components/LazyLoad.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import {
   LazyLoadImage,
-  trackWindowScroll,
+  // trackWindowScroll,
 } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
