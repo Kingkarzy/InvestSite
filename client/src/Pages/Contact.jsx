371130c8fab262ebd09cdc2be3e88a242a45deba
Kingkarzy <43390918+Kingkarzy@users.noreply.github.com>
2023-06-21 08:34:47 +0100

    contact page

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -28,93 +28,96 @@ const Contact = () => {
   };
   return (
     <div
-      id='contact'
-      className='flex flex-col md:flex-row justify-center items-center pt-12 pb-20 md:py-24 px-6 md:px-20 lg:px-32 gap-12 overflow-hidden'
+      id="contact"
+      className="flex flex-col md:flex-row justify-center items-center pt-12 pb-20 md:py-24 px-6 md:px-20 lg:px-32 gap-12 overflow-hidden"
     >
-      <div className='flex flex-col justify-center items-start gap-3'>
-        <h1 className='font-bold text-3xl mt-4'>Send a message.</h1>
-        <p className='mb-5 text-xl font-normal text-left'>
+      <div className="flex flex-col justify-center items-start gap-3">
+        <h1 className="dark:text-black font-bold text-3xl mt-4">
+          Send a message.
+        </h1>
+        <p className="dark:text-black mb-5 text-xl font-normal text-left">
           We always open to hearing your questions
         </p>
-        <div className='flex justify-start items-center gap-12 w-full'>
-          <div className='flex flex-col'>
-            <p className='text-sm font-light'>Mail us at</p>
-            <p className='text-base font-normal'>
+        <div className="flex justify-start items-center gap-12 w-full">
+          <div className="flex flex-col">
+            <p className="text-sm font-light dark:text-black">Mail us at</p>
+            <p className="text-base font-normal dark:text-black">
               support@goobull.com
             </p>
           </div>
-          <div className='flex flex-col'>
-            <p className='text-sm font-light'>Talk to us on</p>
-            <div className='flex text-yellow-450 gap-2 items-center'>
+          <div className="flex flex-col">
+            <p className="text-sm font-light dark:text-black">Talk to us on</p>
+            <div className="flex text-yellow-450 gap-2 items-center">
               <a
-                href='https://facebook.com/'
-                target='blank'
-                className='dark:text-yellow-450 text-purple-950'
+                href="https://facebook.com/"
+                target="blank"
+                className="dark:text-yellow-450 text-purple-950"
               >
                 <FacebookOutlined />
               </a>
               <a
-                href='https://twitter.com/'
-                target='blank'
-                className='dark:text-yellow-450 text-purple-950'
+                href="https://twitter.com/"
+                target="blank"
+                className="dark:text-yellow-450 text-purple-950"
               >
                 <Twitter />
               </a>
               <a
-                href='whatsapp.com/'
-                target='blank'
-                className='dark:text-yellow-400 text-purple-950'
+                href="whatsapp.com/"
+                target="blank"
+                className="dark:text-yellow-400 text-purple-950"
               >
                 <WhatsApp />
               </a>
               <a
-                href='https://telegram.com/'
-                target='blank'
-                className='dark:text-yellow-450 text-purple-950'
+                href="https://telegram.com/"
+                target="blank"
+                className="dark:text-yellow-450 text-purple-950"
               >
                 <Telegram />
               </a>
             </div>
           </div>
         </div>
-        <div className='flex flex-col justify-center'>
-          <h2 className='text-black text-sm font-light mt-5'>
-            Location{' '}
+        <div className="flex flex-col justify-center">
+          <h2 className="text-black text-sm font-light mt-5">Location </h2>
+          <h2 className="dark:text-black">
+            {" "}
+            Puistotie 12, Helsinki 00100, Finland
           </h2>
-          <h2> Puistotie 12, Helsinki 00100, Finland</h2>
         </div>
       </div>
       <div>
         <form
           ref={form}
           onSubmit={sendEmail}
-          className='flex items-center justify-center py-8 md:px-10 black-gradient flex-wrap gap-2 md:gap-5'
+          className="flex items-center justify-center py-8 md:px-10 black-gradient flex-wrap gap-2 md:gap-5"
         >
           <input
-            type='text'
-            name='name'
-            placeholder='Your Full Name'
-            className='w-[85%] bg-transparent text-white text-sm mb-4 border-solid border-b-2 border-[#E0E0E0] focus:outline-none focus:border-b-2 focus:border-purple-950 p-2 rounded-sm'
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            className="w-[85%] bg-transparent text-white text-sm mb-4 border-solid border-b-2 border-[#E0E0E0] focus:outline-none focus:border-b-2 focus:border-purple-950 p-2 rounded-sm"
             required
           />
           <input
-            type='email'
-            name='email'
-            placeholder='Your Email'
-            className='w-[85%] bg-transparent text-white text-sm mb-4 border-solid border-b-2 border-[#E0E0E0] focus:outline-none focus:border-b-2 focus:border-purple-950 p-2 rounded-sm'
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            className="w-[85%] bg-transparent text-white text-sm mb-4 border-solid border-b-2 border-[#E0E0E0] focus:outline-none focus:border-b-2 focus:border-purple-950 p-2 rounded-sm"
             required
           />
           <textarea
-            name='message'
-            rows='4'
-            placeholder='Your Message'
-            className='w-[85%] bg-transparent text-white text-sm mb-4 border-solid border-b-2 border-[#E0E0E0] focus:outline-none focus:border-b-2 focus:border-purple-950 p-2 rounded-sm'
+            name="message"
+            rows="4"
+            placeholder="Your Message"
+            className="w-[85%] bg-transparent text-white text-sm mb-4 border-solid border-b-2 border-[#E0E0E0] focus:outline-none focus:border-b-2 focus:border-purple-950 p-2 rounded-sm"
             required
           ></textarea>
-          <div className='w-[85%] flex justify-end items-center'>
+          <div className="w-[85%] flex justify-end items-center">
             <button
-              type='submit'
-              className='bg-[#e0e0e0] dark:bg-white dark:text-black dark:hover:text-white'
+              type="submit"
+              className="bg-[#e0e0e0] dark:bg-white dark:text-black dark:hover:text-white"
             >
               Send Message
             </button>
