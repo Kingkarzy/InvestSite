e898d9c3b628bfa806b6c4056c2ec429ce453667
ciijay99 <chigozieahanwa99@gmail.com>
2023-06-24 11:58:27 +0100

    Plan bug fixed

--- dashboard/src/pages/Trade/PricingCard.jsx
+++ dashboard/src/pages/Trade/PricingCard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -18,28 +18,6 @@ const PriceCard = ({
   // IMPORT USER STATE
   const user = useSelector((state) => state.user);
   const userId = user._id;
-  const [result, setResult] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `${baseUrl}/api/admin/users/${userId}`,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${user.token}`,
-            },
-          }
-        );
-        setResult(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, [user.token, userId]);
 
   // SET STATES
   const [amount, setAmount] = useState(0);
@@ -67,11 +45,28 @@ const PriceCard = ({
   // HANDLE FORM SUBMISSION
   const handlesubmit = async (e) => {
     e.preventDefault();
+    let balance = 0;
+    try {
+      const response = await axios.get(
+        `${baseUrl}/api/admin/users/${userId}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      balance = response.data.balance;
+      // console.log(balance);
+    } catch (error) {
+      console.log(error);
+    }
+
     if (amount == 0) {
       alert('Amount is required.');
       return;
     }
-    if (price > result.balance) {
+    if (price > balance) {
       return alert('Current Balance Not Enough For This Plan');
     }
     const data = JSON.stringify({
@@ -106,47 +101,51 @@ const PriceCard = ({
 
   return (
     <div
-      className="text-black flex flex-col justify-center items-center border-solid bg-white shadow-xl p-5 mx-auto  transition-all rounded-xl"
+      className='text-black flex flex-col justify-center items-center border-solid bg-white shadow-xl p-5 mx-auto  transition-all rounded-xl'
       style={{
         width: width,
         height: height,
       }}
     >
-      <span className="flex font-semibold scale-[2] mb-5 text-dark dark:text-black">
+      <span className='flex font-semibold scale-[2] mb-5 text-dark dark:text-black'>
         {heading}
       </span>
-      <div className="flex gap-2 ">
-        <h1 className="text-5xl lg:text-5xl font-bold text-center text-dark mb-5">
+      <div className='flex gap-2 '>
+        <h1 className='text-5xl lg:text-5xl font-bold text-center text-dark mb-5'>
           ${price}
         </h1>
-        <span className="font-semibold text-black text-base mt-5">{range}</span>
+        <span className='font-semibold text-black text-base mt-5'>
+          {range}
+        </span>
       </div>
 
-      <p className="mb-3">{percent}% daily</p>
-      <p className="mb-3">Duration: {duration} days</p>
-      <p className="mb-3">{refer}</p>
-      <p className="text-sm mb-5 font-light text-center text-dark">{content}</p>
+      <p className='mb-3'>{percent}% daily</p>
+      <p className='mb-3'>Duration: {duration} days</p>
+      <p className='mb-3'>{refer}</p>
+      <p className='text-sm mb-5 font-light text-center text-dark'>
+        {content}
+      </p>
       <input
-        type="number"
+        type='number'
         required
         min={price}
         max={`${
-          heading == "Silver"
+          heading == 'Silver'
             ? 1000
-            : heading == "Gold"
+            : heading == 'Gold'
             ? 4999
-            : heading == "Diamond"
+            : heading == 'Diamond'
             ? 999999
-            : "Not Valid"
+            : 'Not Valid'
         }`}
-        name="amount"
+        name='amount'
         placeholder={price}
-        className="block w-9/12 h-9 py-1 px-3 text-sm text-gray-600 bg-white border border-solid border-gray-300"
+        className='block w-9/12 h-9 py-1 px-3 text-sm text-gray-600 bg-white border border-solid border-gray-300'
         onChange={handleChange}
       />
       <button
         onClick={handlesubmit}
-        className="bg-indigo-600 mt-6 px-0 py-1 rounded-md w-5/12 text-white hover:scale-110"
+        className='bg-indigo-600 mt-6 px-0 py-1 rounded-md w-5/12 text-white hover:scale-110'
       >
         Join Plan
       </button>
